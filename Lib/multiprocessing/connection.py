03ed5a949b3c0f455d436c6ed1ae4d23d957c854
whokilleddb <whokilleddb@gmail.com>
2022-06-09 01:23:12 +0530

    Minor bug fix

--- Lib/multiprocessing/connection.py
+++ Lib/multiprocessing/connection.py
@@ -80,8 +80,9 @@ def arbitrary_address(family):
             return f"\0listener-{os.getpid()}-{next(_mmap_counter)}"
         return tempfile.NamedTemporaryFile(prefix='listener-', dir=util.get_temp_dir()).name
     elif family == 'AF_PIPE':
-        return tempfile.mktemp(prefix=r'\\.\pipe\pyc-%d-%d-' %
-                               (os.getpid(), next(_mmap_counter)), dir="")
+        return os.path.basename(tempfile.NamedTemporaryFile(
+                                prefix=r'\\.\pipe\pyc-%d-%d-' %
+                                (os.getpid(), next(_mmap_counter)), dir="").name)
     else:
         raise ValueError('unrecognized family')
 
